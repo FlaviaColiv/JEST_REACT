42797ac149e6c24fd07adc608f161acad4a3d386
Flavia Oliveira <flaviacoliv@hotmail.com>
2025-03-04 07:02:04 -0300

    typS 040325 9

--- TYPESCRIPT/index.ts
+++ TYPESCRIPT/index.ts
@@ -4,3 +4,23 @@ function pessoa(params:AnimationEffect) {
 }
 
 pessoa [ 1 ]
+
+// enum TradeType {
+//     ACAO = 9281,
+//     TESOURO_DIRETO = 3221,
+//     TESOURO_SELIC = 6554,
+// }
+
+// enum genero {
+//     M = 'Masculino',
+//     F = 'Feminino',
+// }
+
+enum StatusCode {
+    OK = 200,
+    BadReFquest = 400,
+}
+
+const ok = StatusCode.OK; //200
+const indexOk = StatusCode['OK']; //200
+const stringBadRequest = StatusCode[400]; //BadReFquest
